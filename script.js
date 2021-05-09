3bedf07906356b12ad27e452f9519f5f8be48c9e
Swadesh-Panda <swadeshpanda19@gmail.com>
2021-05-09 19:52:02 +0530

    update1.1

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 var upbutton = document.getElementById("upBtnUp");
+
 var logoimg = document.getElementById("logo");
+
+var myHeader = document.getElementsByTagName("header");
+
+var Lghtmode = document.getElementById("sun");
+var Drkmode = document.getElementById("moon");
 
 window.onscroll = function(){
   if (document.body.scrollTop > 20) {
     upbutton.style.display = "block";
     
-    if (document.body.scrollTop > 500) {
+    if (document.body.scrollTop > 0) {
       logoimg.classList.add("logo");
     }else{
       logoimg.classList.remove("logo");
@@ -15,7 +21,7 @@ window.onscroll = function(){
     upbutton.style.display = "none";
     
     if (document.body.scrollTop > 500) {
-      logoimg.classList.add("logo");
+      logoimg.sty
     }else{
       logoimg.classList.remove("logo");
     }
@@ -24,4 +30,16 @@ window.onscroll = function(){
 
 upbutton.onclick = function(){
   document.body.scrollTop = 0;
+}
+
+Lghtmode.onclick = function(){
+  document.body.style.backgroundColor = "rgba(158, 158, 158, 0.637)";
+  Lghtmode.style.display = "none";
+  Drkmode.style.display = "block";
+}
+
+Drkmode.onclick = function(){
+  document.body.style.backgroundColor = "rgb(63, 63, 63)";
+  Drkmode.style.display = "none";
+  Lghtmode.style.display = "block";
 }
